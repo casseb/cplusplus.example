8015124e4832e61bc443713dd6305165eae832e0
Felipe Soares Casseb dos Santos <s2it_fcasseb@uolinc.com>
2019-09-16 23:12:02 -0300

    Adicionado arquivo do exemplo simples

---
+++ examples/triangulo_simples.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int lado;
+  int altura;
+  int base;
+
+  cout << "Digite o lado do triangulo" << "\n";
+  cin >> lado;
+
+  cout << "Digite a altura do triangulo" << "\n";
+  cin >> altura;
+
+  cout << "Digite a base do triangulo" << "\n";
+  cin >> base;
+
+  cout << "O triangulo possui lado: " << lado << ", altura: " << altura << " e base: " << base << "\n";
+
+  return 0;
+}
